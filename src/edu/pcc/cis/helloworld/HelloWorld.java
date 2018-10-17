ec07441bef3df8a841f912983a7b52621769d46c
Michael Flynn <michael.flynn4@pcc.edu>
2018-10-17 08:07:51 -0700

    Added feature to display Hello, World

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Michael Flynn on 10/17/2018
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
